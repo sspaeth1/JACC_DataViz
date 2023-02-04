970b5f1261412faaecbb2907c68cfe53a93be777
Shlomo spaeth <sam.spaeth@gmail.com>
2023-02-03 17:45:16 -0800

    risk_plot3.js  test data structure v1

---
+++ GBCVD_/data/cleaned/Risk_plot3.js
@@ -0,0 +1,30 @@
+let riskData3 =
+{
+    "Andean Latin America": {
+        "Behavioral": [
+
+            ["x", "Alcoholic cardiomyopathy", "Aortic aneurysm", "Atrial fibrillation and flutter", "Hypertensive heart disease", "Intracerebral hemorrhage", "Ischemic heart disease", "Ischemic stroke", "Lower extremity peripheral arterial disease", "Subarachnoid hemorrhage"],
+            ["Dietary risks", 0, 0.18, 2.93, 118.04, 36.99, 572.08, 43.98, 0.24, 16.13],
+            ["High alcohol use", 0.09, 0, 3.51, 10.14, 24.19, -41.1, 16.51, 0, 0],
+            ["Low physical activity", 0, 0, 0, 0, 0, 10.53, 10.99, 0.11, 0],
+            ["Secondhand smoke", 0, 0, 0, 0, 8.08, 16.81, 4.88, 0, 4.78],
+            ["Smoking", 0, 5.14, 2.9, 0, 29.78, 127.06, 17.13, 0.72, 14.31]
+        ],
+        "Environmental": [
+            ["x", "Alcoholic cardiomyopathy", "Aortic aneurysm", "Atrial fibrillation and flutter", "Hypertensive heart disease", "Intracerebral hemorrhage", "Ischemic heart disease", "Ischemic stroke", "Lower extremity peripheral arterial disease", "Subarachnoid hemorrhage"],
+            ["Dietary risks", 0, 0.18, 2.93, 118.04, 36.99, 572.08, 43.98, 0.24, 16.13],
+            ["High alcohol use", 0.09, 0, 3.51, 10.14, 24.19, -41.1, 16.51, 0, 0],
+            ["Low physical activity", 0, 0, 0, 0, 0, 10.53, 10.99, 0.11, 0],
+            ["Secondhand smoke", 0, 0, 0, 0, 8.08, 16.81, 4.88, 0, 4.78],
+            ["Smoking", 0, 5.14, 2.9, 0, 29.78, 127.06, 17.13, 0.72, 14.31]
+        ],
+        "Metabolic": [
+            ["x", "Aortic aneurysm", "Atrial fibrillation and flutter", "Hypertensive heart disease", "Intracerebral hemorrhage", "Ischemic heart disease", "Ischemic stroke", "Lower extremity peripheral arterial disease", "Subarachnoid hemorrhage"],
+            ["High body-mass index", 0, 10.43, 87.59, 29.47, 193.2, 26.48, 0.84, 18.74, 0],
+            ["High fasting plasma glucose", 0, 0, 0, 16.55, 148.02, 53.88, 1.73, 0, 0],
+            ["High LDL cholesterol", 0, 0, 0, 0, 408.71, 94.5, 0, 0, 0],
+            ["High systolic blood pressure", 1.82, 25.66, 151.19, 165.87, 458.6, 152.54, 0.65, 78.22, 0],
+            ["Kidney dysfunction", 0, 0, 0, 27.07, 115.1, 19.91, 1.2, 0, 0]
+        ]
+    }
+}
