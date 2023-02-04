d97bbb0c5133f0d4fd14d0b5a25d8145d6602b7f
Shlomo spaeth <sam.spaeth@gmail.com>
2023-02-03 20:30:04 -0800

    replaced zero values with null for    bar: { indices: { removeNull: false}   updated correct data

--- GBCVD_/data/cleaned/Risk_plot3.js
+++ GBCVD_/data/cleaned/Risk_plot3.js
@@ -2,29 +2,25 @@ let riskData3 =
 {
     "Andean Latin America": {
         "Behavioral": [
-
-            ["x", "Alcoholic cardiomyopathy", "Aortic aneurysm", "Atrial fibrillation and flutter", "Hypertensive heart disease", "Intracerebral hemorrhage", "Ischemic heart disease", "Ischemic stroke", "Lower extremity peripheral arterial disease", "Subarachnoid hemorrhage"],
-            ["Dietary risks", 0, 0.18, 2.93, 118.04, 36.99, 572.08, 43.98, 0.24, 16.13],
-            ["High alcohol use", 0.09, 0, 3.51, 10.14, 24.19, -41.1, 16.51, 0, 0],
-            ["Low physical activity", 0, 0, 0, 0, 0, 10.53, 10.99, 0.11, 0],
-            ["Secondhand smoke", 0, 0, 0, 0, 8.08, 16.81, 4.88, 0, 4.78],
-            ["Smoking", 0, 5.14, 2.9, 0, 29.78, 127.06, 17.13, 0.72, 14.31]
+            ["Dietary risks", null, 0.18, 2.93, 118.04, 36.99, 572.08, 43.98, 0.24, 16.13],
+            ["High alcohol use", 0.09, null, 3.51, 10.14, 24.19, -41.1, 16.51, null, null],
+            ["Low physical activity", null, null, null, null, null, 10.53, 10.99, 0.11, null],
+            ["Secondhand smoke", null, null, null, null, 8.08, 16.81, 4.88, null, 4.78],
+            ["Smoking", null, 5.14, 2.9, null, 29.78, 127.06, 17.13, 0.72, 14.31]
         ],
         "Environmental": [
-            ["x", "Alcoholic cardiomyopathy", "Aortic aneurysm", "Atrial fibrillation and flutter", "Hypertensive heart disease", "Intracerebral hemorrhage", "Ischemic heart disease", "Ischemic stroke", "Lower extremity peripheral arterial disease", "Myocarditis", "Other cardiomyopathy", "Subarachnoid hemorrhage"],
-            ["Dietary risks", 0, 0.18, 2.93, 118.04, 36.99, 572.08, 43.98, 0.24, 16.13],
-            ["High alcohol use", 0.09, 0, 3.51, 10.14, 24.19, -41.1, 16.51, 0, 0],
-            ["Low physical activity", 0, 0, 0, 0, 0, 10.53, 10.99, 0.11, 0],
-            ["Secondhand smoke", 0, 0, 0, 0, 8.08, 16.81, 4.88, 0, 4.78],
-            ["Smoking", 0, 5.14, 2.9, 0, 29.78, 127.06, 17.13, 0.72, 14.31]
+            ["Ambient particulate matter pollution", null, null, null, null, 62.63, 242.62, 54.98, null, null, null, 30.26],
+            ["High temperature", null, null, null, -0.17, 0.25, -0.34, 0.16, null, null, null, 0.1],
+            ["Household air pollution from solid fuels", null, null, 14.39, 46.74, 11.93, null, null, null, 6.83],
+            ["Lead exposure", null, 0.18, 2.9, 41.89, 23.7, 63.92, 21.29, 0.05, null, null, 10.39],
+            ["Low temperature", null, null, null, 9.15, 22.44, 52.16, 16.33, null, null, null, 11.12],
         ],
         "Metabolic": [
-            ["x", "Aortic aneurysm", "Atrial fibrillation and flutter", "Hypertensive heart disease", "Intracerebral hemorrhage", "Ischemic heart disease", "Ischemic stroke", "Lower extremity peripheral arterial disease", "Subarachnoid hemorrhage"],
-            ["High body-mass index", 0, 10.43, 87.59, 29.47, 193.2, 26.48, 0.84, 18.74, 0],
-            ["High fasting plasma glucose", 0, 0, 0, 16.55, 148.02, 53.88, 1.73, 0, 0],
-            ["High LDL cholesterol", 0, 0, 0, 0, 408.71, 94.5, 0, 0, 0],
-            ["High systolic blood pressure", 1.82, 25.66, 151.19, 165.87, 458.6, 152.54, 0.65, 78.22, 0],
-            ["Kidney dysfunction", 0, 0, 0, 27.07, 115.1, 19.91, 1.2, 0, 0]
+            ["High body-mass index", null, 10.43, 87.59, 29.47, 193.2, 26.48, 0.84, 18.74],
+            ["High fasting plasma glucose", null, null, null, 16.55, 148.02, 53.88, 1.73, 0],
+            ["High LDL cholesterol", null, null, null, null, 408.71, 94.5, null, null],
+            ["High systolic blood pressure", 1.82, 25.66, 151.19, 165.87, 458.6, 152.54, 0.65, 78.22],
+            ["Kidney dysfunction", null, null, null, 27.07, 115.1, 19.91, 1.2, null],
         ]
     }
 }
