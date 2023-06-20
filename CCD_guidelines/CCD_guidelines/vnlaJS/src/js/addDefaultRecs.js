2b40744edc9ec0dba253f5556accae959e0acc06
Shlomo <sam.spaeth@gmail.com>
2023-06-20 13:27:57 -0400

    reorder of default guidelined

--- CCD_guidelines/CCD_guidelines/vnlaJS/src/js/addDefaultRecs.js
+++ CCD_guidelines/CCD_guidelines/vnlaJS/src/js/addDefaultRecs.js
@@ -1,4 +1,10 @@
 function addDefaultRecs(){
+
+    localCategory = 'Nutrition Supplements'
+    createGuidelindCardAndHeader(localCategory)
+    appendElementToCategory(localCategory, "3noBenefit", "Use of nonprescription or dietary supplements is not beneficial (COR 3 No Benefit)");
+    createCardLink(localCategory)
+
     localCategory = 'Antiplatelet Therapy and Oral Anticoagulants'
     createGuidelindCardAndHeader(localCategory)
     appendElementToCategory(localCategory, "1", "See guideline for information on Antiplatelet Therapy and Oral Anticoagulants. ");
@@ -7,12 +13,6 @@ function addDefaultRecs(){
     localCategory = 'Colchicine'
     createGuidelindCardAndHeader(localCategory)
     appendElementToCategory(localCategory, "2b", "Colchicine for secondary prevention may be considered");
-    createCardLink(localCategory)
-
-
-    localCategory = 'Nutrition Supplements'
-    createGuidelindCardAndHeader(localCategory)
-    appendElementToCategory(localCategory, "3", "Use of nonprescription or dietary supplements is not beneficial (COR 3 No Benefit)");
     createCardLink(localCategory)
 
     localCategory = 'Lipid Management'
